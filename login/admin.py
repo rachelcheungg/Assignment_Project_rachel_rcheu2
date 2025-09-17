62d43cc6c0dd5568816e2ddd5f41fed692f9f3f0
rachelcheungg <rachelcheungusa@gmail.com>
2025-09-17 15:24:02 -0500

    created superserver and registered models

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Users, Websites, Favorites
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Websites)
+admin.site.register(Favorites)
