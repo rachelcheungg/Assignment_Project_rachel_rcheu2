98856d99bd71af810468cce1d7b893df1276320c
rachelcheungg <rachelcheungusa@gmail.com>
2025-09-17 17:29:08 -0500

    fixed admin to display models

--- login/admin.py
+++ login/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Users, Websites, Favorites
+from .models import User, Website, Favorite
 # Register your models here.
 
-admin.site.register(Users)
-admin.site.register(Websites)
-admin.site.register(Favorites)
+admin.site.register(User)
+admin.site.register(Website)
+admin.site.register(Favorite)
--- login/models.py
+++ login/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-# Parent Table
-class Users(models.Model):
+
+class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -10,12 +10,11 @@ class Users(models.Model):
     phone_number = models.CharField(max_length=11)
     username = models.CharField(max_length=11)
 
-class Websites(models.Model):
-    website_id = models.AutoField(primary_key=True)
+class Website(models.Model):
+    #website_id = models.AutoField(primary_key=True)
     website_name = models.CharField(max_length=100)
     website_address = models.URLField(max_length=200)
 
-class Favorites(models.Model):
-    pk = models.CompositePrimaryKey("user_id", "website_id")
-    user = models.ForeignKey("Users", on_delete=models.CASCADE)
-    website = models.ForeignKey("Websites", on_delete=models.CASCADE)
+class Favorite(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    website = models.ForeignKey("Website", on_delete=models.CASCADE)
