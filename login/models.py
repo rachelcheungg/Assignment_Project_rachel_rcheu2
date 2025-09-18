3ec12f7b35dba63a26bc50cb8208e8b4bdc0bf06
rachelcheungg <rachelcheungusa@gmail.com>
2025-09-18 13:38:51 -0500

    updated all models and added .txt file

--- login/models.py
+++ login/models.py
@@ -3,12 +3,11 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    username = models.CharField(primary_key=True, max_length=20)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=11)
-    username = models.CharField(max_length=20)
+    phone_number = models.CharField(unique=True, max_length=11, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} : {self.username}"
+        return f"{self.username}"
--- search/models.py
+++ search/models.py
@@ -2,16 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Website(models.Model):
-    website_id = models.AutoField(primary_key=True)
     website_name = models.CharField(max_length=100)
-    website_address = models.URLField(max_length=200)
+    website_address = models.URLField(primary_key=True, unique=True, max_length=200)
 
     def __str__(self):
         return f"{self.website_name} | {self.website_address}"
 
 class Category(models.Model):
-    category_id = models.AutoField(primary_key=True)
-    category_name = models.CharField(max_length=100)
+    category_name = models.CharField(primary_key=True, unique=True, max_length=100)
 
     def __str__(self):
         return f"{self.category_name}"
