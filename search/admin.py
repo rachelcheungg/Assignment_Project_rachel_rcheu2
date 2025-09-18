c3b0a2cd0862f78fec83b5081c9be8278ecf7bbd
rachelcheungg <rachelcheungusa@gmail.com>
2025-09-18 11:53:50 -0500

    added new table to search feature

--- search/admin.py
+++ search/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Website
+from .models import Website, Category
+
 # Register your models here.
 
 admin.site.register(Website)
+admin.site.register(Category)
--- search/models.py
+++ search/models.py
@@ -5,3 +5,7 @@ class Website(models.Model):
     website_id = models.AutoField(primary_key=True)
     website_name = models.CharField(max_length=100)
     website_address = models.URLField(max_length=200)
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=100)
