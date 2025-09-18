599a0b8a5a62431f63d5a13d4239b7b5130813c5
rachelcheungg <rachelcheungusa@gmail.com>
2025-09-18 18:13:22 -0500

    updated models and made migrations

--- search/models.py
+++ search/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Website(models.Model):
-    website_name = models.CharField(max_length=100)
     website_address = models.URLField(primary_key=True, unique=True, max_length=200)
+    website_name = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.website_name} | {self.website_address}"
