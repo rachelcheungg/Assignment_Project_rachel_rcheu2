d29d1d56a380201ed7be75b4141f9038a8e5aa4d
rachelcheungg <rachelcheungusa@gmail.com>
2025-09-18 19:28:29 -0500

    finsihing edits to models and updated notes

--- search/models.py
+++ search/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Website(models.Model):
-    website_address = models.URLField(primary_key=True, unique=True, max_length=200)
-    website_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.website_name} | {self.website_address}"
-
 class Category(models.Model):
     category_name = models.CharField(primary_key=True, unique=True, max_length=100)
 
     def __str__(self):
         return f"{self.category_name}"
+
+
+class Website(models.Model):
+    website_address = models.URLField(primary_key=True, unique=True, max_length=200)
+    website_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.website_name} | {self.website_address}"
