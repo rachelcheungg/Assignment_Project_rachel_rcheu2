05ad2e045d735393291ca320e32dfc93320b4513
rachelcheungg <rachelcheungusa@gmail.com>
2025-09-27 23:11:30 -0500

    built base and generic view for search feature models

--- search/views.py
+++ search/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
+from .models import Category, Website
 
 # Create your views here.
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'search/category_list.html'
+    context_object_name = 'category_list'
+
+
+class WebsiteListView(View):
+    def get(self, request):
+        return render(
+            request,
+            'search/website_list.html',
+            context={'websites': Website.objects.all()}
+        )
+
