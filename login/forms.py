a01690c483216d6b79d38a8712997392e8235e28
rachelcheungg <rachelcheungusa@gmail.com>
2025-10-21 12:32:49 -0500

    created form to add user

---
+++ login/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from login.models import User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = "username", "first_name", "last_name", "email", "phone_number"
+
+    def clean_first_name(self):
+        return self.cleaned_data["first_name"].strip()
+
+    def clean_last_name(self):
+        return self.cleaned_data["last_name"].strip()
